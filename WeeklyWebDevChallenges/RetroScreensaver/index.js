819a49056605cddbe2bab4729d99315954c71483
MarcusAGray <mrcsgry@gmail.com>
2022-01-20 15:14:58 +0000

    added starting code
    Added starting code for the RetroScreenSaver

--- WeeklyWebDevChallenges/RetroScreensaver/index.js
+++ WeeklyWebDevChallenges/RetroScreensaver/index.js
@@ -1,1 +1,7 @@
-console.log("test")
+const screensaver = document.getElementById("screensaver")
+const orb = document.getElementById("orb")
+
+
+function countdown() {
+   // start an (optional) 5 min timer on pageload
+}
